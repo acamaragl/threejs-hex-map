62fc3374b38dffc13b13508554190eac63c14175
Alberto <acamara@game-learn.com>
2018-09-21 14:02:41 +0200

    rename textures and used all

--- examples/basicMap/view.ts
+++ examples/basicMap/view.ts
@@ -1,62 +1,62 @@
 import MapView from '../../src/MapView';
 import { loadFile, loadJSON, loadTexture } from '../../src/util';
 import { TextureAtlas, isMountain, isWater, TileData } from '../../src/interfaces';
-import {generateRandomMap} from "../../src/map-generator"
+import { generateRandomMap } from "../../src/map-generator"
 import { varying } from './util';
 import { TextureLoader } from 'three'
 import { MapMeshOptions } from '../../src/MapMesh';
 
 function asset(relativePath: string): string {
-    return "../../assets/" + relativePath
+  return "../../assets/" + relativePath
 }
 
 async function loadTextureAtlas() {
-    return loadJSON<TextureAtlas>(asset("land-atlas.json"))
+  return loadJSON<TextureAtlas>(asset("land-atlas.json"))
 }
 
 async function generateMap(mapSize: number) {
-    return generateRandomMap(mapSize, (q, r, height) => {
-        const terrain = (height > 0.75 && "mountain") || varying("tundra", "plains","desert","grass","snow","ocean")
-        const trees = !isMountain(height) && !isWater(height) && varying(true, false) ?
-            Math.floor(Math.random()*2) : undefined
-        return {q, r, height, terrain, treeIndex: trees,fog: false, clouds: false }
-    })
+  return generateRandomMap(mapSize, (q, r, height) => {
+    const terrain = (height > 0.75 && "mountain") || varying("text1", "text2", "text3", "text4", "text5", "text6", "text7")
+    const trees = !isMountain(height) && !isWater(height) && varying(true, false) ?
+      Math.floor(Math.random() * 2) : undefined
+    return { q, r, height, terrain, treeIndex: trees, fog: false, clouds: false }
+  })
 }
 
 export async function initView(mapSize: number, initialZoom: number): Promise<MapView> {
-    const textureLoader = new TextureLoader()
-    const loadTexture = (name: string) => textureLoader.load(asset(name))
-    const options: MapMeshOptions = {
-        terrainAtlas: null,
-        terrainAtlasTexture: loadTexture("terrain.png"),
-        hillsNormalTexture: loadTexture("hills-normal.png"),
-        undiscoveredTexture: loadTexture("paper.jpg"),
-        transitionTexture: loadTexture("transitions.png"),
-        treeSpritesheet: loadTexture("rock111.png"),
-        treeSpritesheetSubdivisions: 4
-    }
-    const [map, atlas] = await Promise.all([generateMap(mapSize), loadTextureAtlas()])
-    options.terrainAtlas = atlas
+  const textureLoader = new TextureLoader()
+  const loadTexture = (name: string) => textureLoader.load(asset(name))
+  const options: MapMeshOptions = {
+    terrainAtlas: null,
+    terrainAtlasTexture: loadTexture("terrain.png"),
+    hillsNormalTexture: loadTexture("hills-normal.png"),
+    undiscoveredTexture: loadTexture("paper.jpg"),
+    transitionTexture: loadTexture("transitions.png"),
+    treeSpritesheet: loadTexture("rock111.png"),
+    treeSpritesheetSubdivisions: 4
+  }
+  const [map, atlas] = await Promise.all([generateMap(mapSize), loadTextureAtlas()])
+  options.terrainAtlas = atlas
 
-    if (window.map == null || window.map == undefined) {
-      window.map = map
-      localStorage.setItem('mapData', JSON.parse(JSON.stringify(map)))
-      window.options = options
-      //localStorage.setItem('options', JSON.stringify(options))
-    }else {
-      let mapData=
+  if (window.map == null || window.map == undefined) {
+    window.map = map
+    localStorage.setItem('mapData', JSON.parse(JSON.stringify(map)))
+    window.options = options
+    //localStorage.setItem('options', JSON.stringify(options))
+  } else {
+    let mapData =
       map = window.map
 
-    }
-    const mapView = new MapView()
-    mapView.zoom = initialZoom
-    mapView.load(map, options)
-    const controller = mapView.controller as DefaultMapViewController
-    controller.debugOutput = document.getElementById("coordinates") as HTMLElement
-    mapView.onTileSelected = (tile: TileData) => {
-      // uncover tiles around initial selection
-    }
-    return mapView
+  }
+  const mapView = new MapView()
+  mapView.zoom = initialZoom
+  mapView.load(map, options)
+  const controller = mapView.controller as DefaultMapViewController
+  controller.debugOutput = document.getElementById("coordinates") as HTMLElement
+  mapView.onTileSelected = (tile: TileData) => {
+    // uncover tiles around initial selection
+  }
+  return mapView
 }
 
 /**
@@ -66,13 +66,13 @@ export async function initView(mapSize: number, initialZoom: number): Promise<Ma
  * @param tile tile around which fog should be updated
  */
 function setFogAround(mapView: MapView, tile: TileData, range: number, fog: boolean, clouds: boolean) {
-    const tiles = mapView.getTileGrid().neighbors(tile.q, tile.r, range)
+  const tiles = mapView.getTileGrid().neighbors(tile.q, tile.r, range)
 
-    const updated = tiles.map(t => {
-        t.fog = fog
-        t.clouds= clouds
-        return t
-    })
+  const updated = tiles.map(t => {
+    t.fog = fog
+    t.clouds = clouds
+    return t
+  })
 
-    mapView.updateTiles(updated)
+  mapView.updateTiles(updated)
 }
